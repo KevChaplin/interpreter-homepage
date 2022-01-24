cb4fa4ec5c3b5e48b675ba94b2e9cb07d588c7c9
KevChaplin <kevchaplin44@gmail.com>
2022-01-24 13:12:57 +0900

    about me section added, styling made uniform, code refactored

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import * as React from 'react';
 
+import Box from '@mui/material/Box';
+
 import ButtonAppBar from './components/App-bar/App-bar'
 import Intro from './components/Intro'
+import AboutMe from './components/About-me'
 import MyWork from './components/My-work'
 import Copyright from './components/Copyright'
 import Contact from './components/Contact'
 
+import introBackgroundImg from './images/intro_background.jpg'
+
 export default function App() {
   return (
-    <React.Fragment>
+    <Box sx={{
+      backgroundImage: `url(${introBackgroundImg})`,
+      backgroundSize: 'cover'}}>
       <ButtonAppBar />
       <Intro />
+      <AboutMe />
       <MyWork />
       <Contact />
       <Copyright />
-    </React.Fragment>
+    </Box>
   );
 }
