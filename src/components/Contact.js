cfec08878321eac1205c6f7c4ffeb36d860481e1
KevChaplin <kevchaplin44@gmail.com>
2022-01-31 11:48:18 +0900

    added alert message on message submit regarding site is in production

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,9 +7,12 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField'
 import { Typography } from '@mui/material';
+import Alert from '@mui/material/Alert';
+import Collapse from '@mui/material/Collapse';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 
 import { useTranslation } from 'react-i18next'
-
 
 // Contact Form
 export default function Contact() {
@@ -22,27 +25,56 @@ export default function Contact() {
     
     // Contact form submitted using formatsubmit.io
     // Yet to be updated with client's email account
-    function handleSubmit(e) {
-        e.preventDefault()
-        fetch("https://formsubmit.co/ajax/d19908dd31e0ee7758db5f46e8fc1476", {
-            method: "POST",
-            headers: { 
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({
-                name: {name},
-                email: {email},
-                message: {message}
-            })
-        })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
+    // Code to be added once site is live and owner has registered with formsubmit
+    // function handleSubmit(e) {
+      
+        // e.preventDefault()
+        // fetch("https://formsubmit.co/ajax/d19908dd31e0ee7758db5f46e8fc1476", {
+        //     method: "POST",
+        //     headers: { 
+        //         'Content-Type': 'application/json',
+        //         'Accept': 'application/json'
+        //     },
+        //     body: JSON.stringify({
+        //         name: {name},
+        //         email: {email},
+        //         message: {message}
+        //     })
+        // })
+        // .then(response => response.json())
+        // .then(data => console.log(data))
+        // .catch(error => console.log(error));
+    // }
+
+    // for alert message
+    const [open, setOpen] = React.useState(false);
+
+    function handleSubmit() {
+        setOpen(true)
     }
 
     return (
         <Card id="contact-me" sx={{maxWidth:1000, my:5, mx: "auto", py: 3, px: [2, 5]}}>
+            <Collapse in={open}>
+                <Alert
+                    severity="warning"
+                    action={
+                        <IconButton
+                            aria-label="close"
+                            color="inherit"
+                            size="small"
+                            onClick={() => {
+                                setOpen(false);
+                            }}
+                        >
+                            <CloseIcon fontSize="inherit" />
+                        </IconButton>
+                    }
+                    sx={{ mb: 2 }}
+                >
+                   Sorry, we could not send your message. This site is under construction, please vist <a href="http://inter.zacharski.com/">My Current Homepage</a> for full contact details.
+                </Alert>
+            </Collapse>
             <CardContent>
                 <Typography variant="h2" gutterBottom>
                     {t('contact.title')}
